ccba983dc8fc4d3a33ce12060a7854037dde609b
sbangariGIT <bangarisairohan@gmail.com>
2025-02-19 20:00:57 +0530

    bug fix

--- app/slacklog.py
+++ app/slacklog.py
@@ -9,7 +9,7 @@ class Logger:
         self.slack_client = WebClient(os.getenv('SLACK_BOT_TOKEN'))
 
     def notify_slack(self, message):
-        self.debug_print(f"Sending Slack Message: {message}")
+        print(f"Sending Slack Message: {message}")
         message = f"[AI_PHONE_ASSISTANT] " + message
         self.slack_client.chat_postMessage(
         channel='monitor-cloud',
